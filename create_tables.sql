36bb5876a70479f7782321c2d940c147e2ceb555
AkessonHenrik <henrik.akesson@outlook.com>
2020-02-14 14:18:27 +0100

    Initial commit

---
+++ create_tables.sql
@@ -0,0 +1,12 @@
+-- Database: streamus
+
+-- DROP DATABASE streamus;
+
+CREATE DATABASE streamus
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
