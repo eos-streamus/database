9ef516bad88a532cb6ccfdfd39860c4af343a32b
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-02-23 18:57:22 +0100

    Update test data

--- test_data.sql
+++ test_data.sql
@@ -4,29 +4,29 @@
 
 -- Album
 
-select createepisode('dulcinea.mp4', 'Dulcinea', 2700, 2, 1::smallint, 1::smallint);
-select createepisode('theBigEmpty.mp4', 'The Big Empty', 2700, 2, 1::smallint, 2::smallint);
-select createepisode('rememberTheCant.mp4', 'Remember the Cant', 2700, 2, 1::smallint, 3::smallint);
-select createepisode('CQB.mp4', 'CQB (Close Quarter Battle)', 2700, 2, 1::smallint, 4::smallint);
-select createepisode('backToTheButcher.mp4', 'Back to the Butcher', 2700, 2, 1::smallint, 5::smallint);
-select createepisode('rockBottom.mp4', 'Rock Bottom', 2700, 2, 1::smallint, 6::smallint);
-select createepisode('windmills.mp4', 'Windmills', 2700, 2, 1::smallint, 7::smallint);
-select createepisode('salvage.mp4', 'Salvage', 2700, 2, 1::smallint, 8::smallint);
-select createepisode('criticalMass.mp4', 'Critical Mass', 2700, 2, 1::smallint, 9::smallint);
-select createepisode('leviathanWakes.mp4', 'Leviathan Wakes', 2700, 2, 1::smallint, 10::smallint);
+select createepisode('dulcinea.mp4', 'Dulcinea', 2700, 39, 1::smallint, 1::smallint);
+select createepisode('theBigEmpty.mp4', 'The Big Empty', 2700, 39, 1::smallint, 2::smallint);
+select createepisode('rememberTheCant.mp4', 'Remember the Cant', 2700, 39, 1::smallint, 3::smallint);
+select createepisode('CQB.mp4', 'CQB (Close Quarter Battle)', 2700, 39, 1::smallint, 4::smallint);
+select createepisode('backToTheButcher.mp4', 'Back to the Butcher', 2700, 39, 1::smallint, 5::smallint);
+select createepisode('rockBottom.mp4', 'Rock Bottom', 2700, 39, 1::smallint, 6::smallint);
+select createepisode('windmills.mp4', 'Windmills', 2700, 39, 1::smallint, 7::smallint);
+select createepisode('salvage.mp4', 'Salvage', 2700, 39, 1::smallint, 8::smallint);
+select createepisode('criticalMass.mp4', 'Critical Mass', 2700, 39, 1::smallint, 9::smallint);
+select createepisode('leviathanWakes.mp4', 'Leviathan Wakes', 2700, 39, 1::smallint, 10::smallint);
 
 
-select createepisode('safe.mp4', 'Safe', 2700, 2, 2::smallint, 1::smallint);
-select createepisode('doorsAndCorners.mp4', 'Doors & Corners', 2700, 2, 2::smallint, 2::smallint);
-select createepisode('static.mp4', 'Static', 2700, 2, 2::smallint, 3::smallint);
-select createepisode('godspeed.mp4', 'Godspeed', 2700, 2, 2::smallint, 4::smallint);
-select createepisode('home.mp4', 'Home', 2700, 2, 2::smallint, 5::smallint);
-select createepisode('paradigmShift.mp4', 'Paradigm Shift', 2700, 2, 2::smallint, 6::smallint);
-select createepisode('theSeventhMan.mp4', 'The Seventh Man', 2700, 2, 2::smallint, 7::smallint);
-select createepisode('pyre.mp4', 'Pyre', 2700, 2, 2::smallint, 8::smallint);
-select createepisode('theWeepingSomnambulist.mp4', 'The Weeping Somnambulist', 2700, 2, 2::smallint, 9::smallint);
-select createepisode('cascade.mp4', 'Cascade', 2700, 2, 2::smallint, 10::smallint);
-select createepisode('hereThereBeDragons.mp4', 'Here There be Dragons', 2700, 2, 2::smallint, 11::smallint);
-select createepisode('theMonsterAndTheRocket.mp4', 'The Monster and the Rocket', 2700, 2, 2::smallint, 12::smallint);
-select createepisode('calibansWar.mp4', 'Caliban''s War', 2700, 2, 2::smallint, 13::smallint);
+select createepisode('safe.mp4', 'Safe', 2700, 39, 2::smallint, 1::smallint);
+select createepisode('doorsAndCorners.mp4', 'Doors & Corners', 2700, 39, 2::smallint, 2::smallint);
+select createepisode('static.mp4', 'Static', 2700, 39, 2::smallint, 3::smallint);
+select createepisode('godspeed.mp4', 'Godspeed', 2700, 39, 2::smallint, 4::smallint);
+select createepisode('home.mp4', 'Home', 2700, 39, 2::smallint, 5::smallint);
+select createepisode('paradigmShift.mp4', 'Paradigm Shift', 2700, 39, 2::smallint, 6::smallint);
+select createepisode('theSeventhMan.mp4', 'The Seventh Man', 2700, 39, 2::smallint, 7::smallint);
+select createepisode('pyre.mp4', 'Pyre', 2700, 39, 2::smallint, 8::smallint);
+select createepisode('theWeepingSomnambulist.mp4', 'The Weeping Somnambulist', 2700, 39, 2::smallint, 9::smallint);
+select createepisode('cascade.mp4', 'Cascade', 2700, 39, 2::smallint, 10::smallint);
+select createepisode('hereThereBeDragons.mp4', 'Here There be Dragons', 2700, 39, 2::smallint, 11::smallint);
+select createepisode('theMonsterAndTheRocket.mp4', 'The Monster and the Rocket', 2700, 39, 2::smallint, 12::smallint);
+select createepisode('calibansWar.mp4', 'Caliban''s War', 2700, 39, 2::smallint, 13::smallint);
 
